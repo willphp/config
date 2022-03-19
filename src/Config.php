5c23526676bcc28a63e14cf0fd4be83b11a480e1
willphp <24203741@qq.com>
2022-03-19 23:57:07 +0800

    new update

--- src/Config.php
+++ src/Config.php
@@ -3,7 +3,7 @@
  | Software: [WillPHP framework]
  | Site: www.113344.com
  |--------------------------------------------------------------------------
- | Author: NoMind <24203741@qq.com>
+ | Author: no-mind <24203741@qq.com>
  | WeChat: www113344
  | Copyright (c) 2020-2022, www.113344.com. All Rights Reserved.
  |-------------------------------------------------------------------------*/
